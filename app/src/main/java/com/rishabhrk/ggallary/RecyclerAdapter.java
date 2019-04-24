7e19224b2c71e7f07f2fdcbb98dea52748a18167
Nikhil Karnwal <nikhilkarnwal@yahoo.com>
2019-04-25 00:51:44 +0530

    added code to generate and bitmap

--- app/src/main/java/com/rishabhrk/ggallary/RecyclerAdapter.java
+++ app/src/main/java/com/rishabhrk/ggallary/RecyclerAdapter.java
@@ -29,9 +29,47 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.imageV
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerAdapter.imageViewHolder imageViewHolder, int i) {
-        int s= filepath.indexOf(i);
-        imageViewHolder.album.setImageResource(s);
+        imageViewHolder.album.setImageBitmap(decodeSampledBitmapFromResource(filepath.get(i), 100, 100));
 
+    }
+
+    private Bitmap decodeSampledBitmapFromResource(String filePath,
+                                                   int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+        BitmapFactory.decodeFile(filePath, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(filePath, options);
+    }
+    private int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 
     @Override
